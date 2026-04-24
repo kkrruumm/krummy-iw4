f1f3d5b4f45f145f9b88323e9c93c09375693666
krum <git@kris.sh>
2026-04-24 11:22:53 -0500

    split logging

--- startserver.sh
+++ startserver.sh
@@ -3,11 +3,12 @@
 # krummy iw4x server startup file
 
 name=$(uname -n) # each container has an assigned hostname corresponding to what kind of server it's running
+
 case "$name" in
-    iw46v6tdm) GamePort=28960 && ServerFilename=tdm.cfg ;;
-    iw46v6snd) GamePort=28961 && ServerFilename=snd.cfg ;;
-    iw48pgg) GamePort=28962 && ServerFilename=gg.cfg ;;
-    iw4gtnw) GamePort=28963 && ServerFilename=gtnw.cfg ;;
+    iw46v6tdm) GamePort=28960 && ServerFilename=tdm.cfg && HomePath=/mnt/homepaths/6v6tdm ;;
+    iw46v6snd) GamePort=28961 && ServerFilename=snd.cfg && HomePath=/mnt/homepaths/6v6snd ;;
+    iw48pgg)   GamePort=28962 && ServerFilename=gg.cfg  && HomePath=/mnt/homepaths/8pgg   ;;
+    iw4gtnw)   GamePort=28963 && ServerFilename=gtnw.cfg && HomePath=/mnt/homepaths/gtnw  ;;
     *) printf '%s\n' 'unknown hostname, where am i?' ; exit 1 ;;
 esac
 
@@ -17,9 +18,13 @@ esac
 LANMode=0
 
 # Enable logging
-LogFile=0
+LogFile=2
 
-# move to the directory that has MW2 files 
+# make sure this server's homepath exists
+mkdir -p "$HomePath" ||
+    { printf '%s\n' "failed to create homepath $HomePath, cannot continue" ; exit 1 ; }
+
+# move to the directory that has MW2 files
 cd /mnt ||
     { printf '%s\n' 'failed to move to /mnt, cannot continue' ; exit 1 ; }
 
@@ -35,4 +40,4 @@ fi
 type wine > /dev/null ||
     { printf '%s\n' 'wine not found in PATH, is it installed?' ; exit 1 ; }
 
-wine iw4x.exe -dedicated -stdout +set fs_game "$ModFolderName" +set sv_lanonly "$LANMode" +set net_port "$GamePort" +exec "$ServerFilename" +set logfile "$LogFile" +set party_enable "0" +map_rotate
+wine iw4x.exe -dedicated -stdout +set fs_homepath "$HomePath" +set fs_game "$ModFolderName" +set sv_lanonly "$LANMode" +set net_port "$GamePort" +exec "$ServerFilename" +set logfile "$LogFile" +set party_enable "0" +map_rotate
